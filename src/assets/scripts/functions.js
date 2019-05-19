c1e35a9525f085e5ef11eed9e284acae31c2bd74
unknown <milosavramovic1990@gmail.com>
2019-05-19 14:04:00 +0200

    funkcije

--- src/assets/scripts/functions.js
+++ src/assets/scripts/functions.js
@@ -1,10 +1,29 @@
-function plata() {
- //let ime = document.getElementById('p1ID').textContent;
- //let prezime =document.getElementById('p2ID').textContent;
- // let adresa = document.getElementById('p3ID').textContent;
-  let neto = parseFloat(document.getElementById('p4ID').value);
-  let bruto = parseFloat(document.getElementById('p5ID').value = neto * 1, 37);
-  document.getElementById('obracunID').innerHTML = `Bruto zarada je:' + ${racun};
+export default function plata() {
+  const neto = parseFloat(document.getElementById('neto').value);
+  const bruto = parseFloat((document.getElementById('bruto').value = neto * (137 / 100)));
+}
+
+export default function dodajRed() {
+  const ime = document.getElementById('ime');
+  const prezime = document.getElementById('prezime');
+  const adresa = document.getElementById('adresa');
+  const neto = document.getElementById('neto').value;
+  const bruto = document.getElementById('bruto').value = neto * (137 / 100);
+
+  const table = document.getElementsByTagName('table')(0);
+  const newRow = table.insertRow(table.rows.length);
+
+  const cell_1 = newRow.insertCell(0);
+  const cell_2 = newRow.insertCell(1);
+  const cell_3 = newRow.insertCell(2);
+  const cell_4 = newRow.insertCell(3);
+  const cell_5 = newRow.insertCell(4);
+
+  cell_1.innerHTML = ime;
+  cell_2.innerHTML = prezime;
+  cell_3.innerHTML = adresa;
+  cell_4.innerHTML = neto;
+  cell_5.innerHTML = bruto;
 };
 
-export {plata};
+
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,7 +1,12 @@
 import 'bootstrap';
 import '../styles/index.scss';
-import { plata } from './functions';
+import plata from './functions';
+import dodajRed from './functions';
 
 window.plata = function() {
   plata();
+};
+
+window.dodajRed = function() {
+  dodajRed();
 };
