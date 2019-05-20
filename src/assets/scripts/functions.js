0ec67edc651a2ae703adabcfa07b3ffbad8be405
unknown <milosavramovic1990@gmail.com>
2019-05-20 12:05:46 +0200

    funkcije

--- src/assets/scripts/functions.js
+++ src/assets/scripts/functions.js
@@ -2,28 +2,84 @@ export default function plata() {
   const neto = parseFloat(document.getElementById('neto').value);
   const bruto = parseFloat((document.getElementById('bruto').value = neto * (137 / 100)));
 }
-
+/*
 export default function dodajRed() {
-  const ime = document.getElementById('ime');
-  const prezime = document.getElementById('prezime');
-  const adresa = document.getElementById('adresa');
-  const neto = document.getElementById('neto').value;
-  const bruto = document.getElementById('bruto').value = neto * (137 / 100);
+  let ime     = document.getElementById('ime').value;
+  let prezime = document.getElementById('prezime').value;
+  let adresa  = document.getElementById('adresa').value;
+  let neto    = document.getElementById('neto').value;
+  let bruto   = document.getElementById('bruto').value;
 
-  const table = document.getElementsByTagName('table')(0);
-  const newRow = table.insertRow(table.rows.length);
+  let tabela  = document.getElementById('table')(0);
+  let noviRed = tabela.insertRow(table.rows.length);
 
-  const cell_1 = newRow.insertCell(0);
-  const cell_2 = newRow.insertCell(1);
-  const cell_3 = newRow.insertCell(2);
-  const cell_4 = newRow.insertCell(3);
-  const cell_5 = newRow.insertCell(4);
+  let kolona_1 = noviRed.insertCell(0);
+  let kolona_2 = noviRed.insertCell(1);
+  let kolona_3 = noviRed.insertCell(2);
+  let kolona_4 = noviRed.insertCell(3);
+  let kolona_5 = noviRed.insertCell(4);
 
-  cell_1.innerHTML = ime;
-  cell_2.innerHTML = prezime;
-  cell_3.innerHTML = adresa;
-  cell_4.innerHTML = neto;
-  cell_5.innerHTML = bruto;
+  kolona_1.innerHTML = ime;
+  kolona_2.innerHTML = prezime;
+  kolona_3.innerHTML = adresa;
+  kolona_4.innerHTML = neto;
+  kolona_5.innerHTML = bruto;
 };
+*/
+/*
+function dodajRed() {
+  const tabela = document.getElementById('tabela');
+  let podatak;
+  let mojInput;
+  const red = document.createElement('tr');
+  red.setAttribute('class', 'unit-table');
 
+  for (let i = 0; i < 5; i++) {
+    podatak = document.createElement('td');
+    mojInput = document.createElement('input');
+    mojInput.setAttribute('type', 'text');
+    podatak.appendChild(mojInput);
+    red.appendChild(podatak);
+  }
 
+  tabela.appendChild(red);
+}
+*/
+
+/*
+function dodajRed() {
+  let tabela = document.getElementById('tabela');
+  tabela.appendChild(red);
+  let ime = document.getElementById('ime');
+  ime = document.createElement('td');
+  let prezime = document.getElementById('prezime');
+  prezime = document.createElement('td');
+  let adresa = document.getElementById('adresa');
+  adresa = document.createElement('td');
+  let neto = document.getElementById('neto');
+  neto = document.createElement('td');
+  let bruto = document.getElementById('bruto');
+  bruto = document.createElement('td');
+  let podatak = document.createElement(td);
+  red.appendChild(podatak);
+
+  if (ime != '') {
+    var ime = input('Ime: ');
+    var prezime = input('Prezime: ');
+    var adresa = input('Adresa: ');
+    var neto = input('Neto: ');
+  }
+
+  this.tabela.push(
+    { ime: ime },
+    { prezime: prezime },
+    { adresa: adresa },
+    { neto: neto },
+    { bruto: bruto }
+  );
+
+  document.getElementById('dodajID').onclick = dodajRed;
+}
+*/
+
+export { plata };
