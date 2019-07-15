34fe1777b78c9cb496e6339852b2bc1f884e39c8
unknown <milosavramovic1990@gmail.com>
2019-07-15 18:23:52 +0200

    funkcije

--- src/assets/scripts/functions.js
+++ src/assets/scripts/functions.js
@@ -1,18 +1,21 @@
-export default function plata() {
+function plata() {
   const neto = parseFloat(document.getElementById('neto').value);
   const bruto = parseFloat((document.getElementById('bruto').value = neto * (137 / 100)));
 }
-
-const red = 1;
 const dodaj = document.getElementById('dodajID');
 dodaj.addEventListener('click', dodajRed);
 
 function dodajRed() {
+  plata();
+
+  const red = 1;
   const ime = document.getElementById('ime').value;
   const prezime = document.getElementById('prezime').value;
   const adresa = document.getElementById('adresa').value;
   const neto = document.getElementById('neto').value;
   const bruto = document.getElementById('bruto').value;
+  const izmeni = document.getElementById('izmeni').value;
+  const izbrisi = document.getElementById('izbrisi').value;
 
   const tabela = document.getElementById('tabela');
 
@@ -23,14 +26,16 @@ function dodajRed() {
   const kol_3 = noviRed.insertCell(2);
   const kol_4 = noviRed.insertCell(3);
   const kol_5 = noviRed.insertCell(4);
+  const kol_6 = noviRed.insertCell(5);
+  const kol_7 = noviRed.insertCell(6);
 
   kol_1.innerHTML = ime;
   kol_2.innerHTML = prezime;
   kol_3.innerHTML = adresa;
   kol_4.innerHTML = neto;
   kol_5.innerHTML = bruto;
-
-  red++;
+  kol_6.innerHTML = izmeni;
+  kol_7.innerHTML = izbrisi;
 }
 
 export { plata, dodajRed };
